45cfa34ad4103d3d7cfc9a3300181c5b53ec9f42
Arjun Lall <lallarjun@gmail.com>
2017-06-01 14:38:13 +0000

    fixed server to keep track count

--- server.js
+++ server.js
@@ -15,23 +15,35 @@ const server = express()
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
-
+let counter = 0;
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+const updateCount = (count) => {
+  wss.clients.forEach((c) => {
+    c.send(JSON.stringify({type: "count", counter: count}))
+  })
+}
+
+const broadcast = (msg) => {
+  wss.clients.forEach((c) => {
+    if(c != client) {
+      c.send(JSON.stringify(msg));
+    }
+  })
+}
 
 wss.on('connection', (client, req) => {
   console.log('Client connected');
-
+  counter += 1;
+  updateCount(counter);
+  // At this point in time wss.clients is an array that includes
+  // the ws objects of all clients, including the one who just connected
   //helper function to broadcast messages
-  const broadcast = (msg) => {
-    wss.clients.forEach((c) => {
-      if(c != client) {
-        c.send(JSON.stringify(msg));
-      }
-    })
-  }
 
+
+
+  //when messaqge sent from client
   client.on('message', (msg) => {
     msgData = JSON.parse(msg)
     switch(msgData.type){
@@ -53,6 +65,12 @@ wss.on('connection', (client, req) => {
   })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  client.on('close', () => console.log('Client disconnected'));
+  client.on('close', () => {
+    console.log('Client disconnected')
+    counter -= 1;
+    updateCount(counter);
 
+  });
+    // At this point in time wss.clients no longer contains the ws object
+    // of the client who disconnected
 });
